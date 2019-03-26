7085b1f107165e1005b9cd4ac0be6b5135e6ddc0
alireza <easazade@gmail.com>
2019-03-26 15:39:51 +0430

    running some migrations for job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
 class job(models.Model):
-    image = models.ImageField(upload_to='')
+    image = models.ImageField(upload_to='job')
     summary = models.CharField(max_length=200)
