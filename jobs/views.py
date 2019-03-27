6d98a923d8f911b54ccb479ad4701e5931b21503
alireza <easazade@gmail.com>
2019-03-27 08:02:18 +0430

    adding homepage

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request: HttpRequest):
+    return render(request, 'jobs/home.html')
+
+
+def some(request: HttpRequest):
+    return HttpResponse()
